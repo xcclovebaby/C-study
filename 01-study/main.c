8a969d7e9e3ca04f58e95b140488ed2a84e4bf8f
chcc <601553044@qq.com>
2022-07-06 09:55:11 +0800

    first commit

---
+++ 01-study/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h> //头文件包含,一定要有
+
+/* qt下载地址: https://download.qt.io/archive/qt/5.9/5.9.0 */
+
+int main() //main函数是主函数,任意一个c程序必须只能有一个主函数,是程序的入口
+{
+    printf("Hello World!\n"); //输出hello world
+    return 0; //给函数设置一个返回值
+}
